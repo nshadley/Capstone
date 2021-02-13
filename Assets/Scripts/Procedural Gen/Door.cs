9fc87db4dd0c6f2327ceaa3645d219cbd032fc3a
Nicole <nmshadley@gmail.com>
2021-02-12 18:33:48 -0600

    Start of room and door code

---
+++ Assets/Scripts/Procedural Gen/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public bool connected = false;
+    public enum DoorDirection {Up, Down, Left, Right};
+    public DoorDirection doorDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Procedural Gen/Room.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField]
+    protected bool puzzle1 = false;
+    [SerializeField]
+    protected bool puzzle2 = false;
+
+    public enum RoomDirection { North, East, South, West};
+    public RoomDirection RoomDir;
+
+    protected bool doorUp = false;
+    protected bool doorDown = false;
+    protected bool doorRight = false;
+    protected bool doorLeft = false;
+
+
+    void Awake()
+    {
+        Door door = GetComponentInChildren<Door>();
+        if (door.doorDir == Door.DoorDirection.Up)
+            doorUp = true;
+        if (door.doorDir == Door.DoorDirection.Down)
+            doorDown = true;
+        if (door.doorDir == Door.DoorDirection.Left)
+            doorLeft = true;
+        if (door.doorDir == Door.DoorDirection.Right)
+            doorRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
