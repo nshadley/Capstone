fc59fecfab95b4a2c38d1fda88b59f8bf8007cf3
Nicole <nmshadley@gmail.com>
2021-02-22 22:34:03 -0600

    Start of room generator

---
+++ Assets/Scripts/Procedural Gen/Generator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    [SerializeField]
+    int NumberOfRooms = 10;
+
+    [SerializeField]
+    GameObject[] availableRooms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateRooms()
+    {
+
+    }
+}
